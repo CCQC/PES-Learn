42b3276c83690d25054b6f9a4950b35be113bca5
adabbott <adabbott@uga.edu>
2018-01-26 10:14:58 -0500

    Added codecov, travis yml's, python setup, and git ignore

---
+++ setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='molssi',
+        version="0.1.1",
+        description='A project in collaboration with the Molecular Sciences Software Institute',
+        author='Adam Abbott',
+        author_email='adabbott@uga.edu',
+        url="https://github.com/adabbott/molssi",
+        license='BSD-3C',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'numpy>=1.7',
+        ],
+        extras_require={
+            'docs': [
+                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
+                'sphinxcontrib-napoleon',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ],
+            'tests': [
+                'pytest',
+                'pytest-cov',
+                'pytest-pep8',
+                'tox',
+            ],
+        },
+
+        tests_require=[
+            'pytest',
+            'pytest-cov',
+            'pytest-pep8',
+            'tox',
+        ],
+
+        classifiers=[
+            'Development Status :: 1 - Planning',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+        ],
+        zip_safe=True,
+    )
