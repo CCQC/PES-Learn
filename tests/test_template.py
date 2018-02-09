85581f419f2b413c65ea457b85d04f80df24d0c9
adabbott <adabbott@uga.edu>
2018-02-09 17:14:29 -0500

    added more methods for processing xyz template input files, and tests for methods

--- tests/test_template.py
+++ tests/test_template.py
@@ -6,6 +6,7 @@ import molssi
 import pytest
 import re
 
+# open an xyz style input file as file string
 with open('tests/xyz_template.dat', 'r') as f:
     testfile = f.read()
 
@@ -14,5 +15,15 @@ template_object = molssi.template.TemplateProcessor(testfile)
 def test_extract_xyz():
     x = template_object.extract_xyz()
     assert re.match(molssi.regex.xyz_block_regex, x)
-    
-    
+   
+def test_header_xyz():
+    x = template_object.header_xyz()
+    assert x == '# a psi4 input file to text xyz extraction\n\nmolecule test {\n0 1\n'    
+
+def test_footer_xyz():
+    x = template_object.footer_xyz()
+    assert x == "}\n\nset basis cc-pvdz\nset reference rhf\n\nenergy('hf')\n"
+
+def test_parse_xyz():
+    x, y  = template_object.parse_xyz()
+    assert ((x == 64) and (y == 115))
