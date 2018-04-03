56879c3fa901457de53116243a9a618504ea02cb
adabbott <adabbott@uga.edu>
2018-04-03 19:55:19 -0400

    added some more constants

--- molssi/constants.py
+++ molssi/constants.py
@@ -1,3 +1,10 @@
-# some physical constants used in the code
+import math
+
+# some constants used in the code
+rad2deg = 180.0 / math.pi
+deg2rad = math.pi / 180.0
+
 bohr2angstroms = 0.52917720859
 hartree2ev =  27.21138505
+
+
