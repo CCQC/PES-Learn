05ab48e014f3175c25b70d6362dd858020daf9c7
adabbott <adabbott@uga.edu>
2018-04-05 12:28:42 -0400

    added initial sketch of InputProcessor class

---
+++ molssi/input_processor.py
@@ -0,0 +1,11 @@
+"""
+A class for extracting information from the main input of the user
+"""
+
+class InputProcessor(object):
+    pass
+
+
+# method: extract zmat string with regex
+# method: extract internal coordinate scan ranges r1 = [0.7, 3.0], etc
+# method(s): keep track of keyword options used  
