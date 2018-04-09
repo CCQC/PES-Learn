2f12a6df5096f1a546180a604fcd9bb39ace451e
adabbott <adabbott@uga.edu>
2018-04-09 18:25:50 -0400

    added initial draft of zmat and internal coordinate ranges extraction

--- molssi/input_processor.py
+++ molssi/input_processor.py
@@ -2,9 +2,34 @@
 A class for extracting information from the main input of the user
 """
 
-class InputProcessor(object):
-    pass
+from regex import intcoords_regex
+import re
+import molecule 
+import ast
 
+class InputProcessor(object):
+    """
+    """
+    def __init__(self, input_path):
+        self.input_path = input_path
+        self.zmat_string = extract_zmat_string(self.input_path)
+
+    def extract_zmat_string(self):
+        with open(self.input_path, 'r') as f:
+            self.full_string = f.read() 
+        return re.findall(intcoords_regex, full_string)[0] 
+
+    def extract_intcos_ranges(self):
+        # create molecule object to obtain coordinate labels
+        mol = Molecule(self.zmat_string)
+        geomlabels = mol.geom_parameters 
+        ranges = {}
+        # for every geometry label look for its range identifer, e.g. R1 = [0.5, 1.2]
+        for label in geomlabels:
+            match = re.search(label+"\s*=\s*(\[.+\])", self.full_string).group()
+            ranges[label] = ast.literal_eval(match)
+         
+        
 
 # method: extract zmat string with regex
 # method: extract internal coordinate scan ranges r1 = [0.7, 3.0], etc
