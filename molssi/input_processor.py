ca39f3169181a322fb53aa86c98927103f556a06
adabbott <adabbott@uga.edu>
2018-04-10 15:05:58 -0400

    using ordered dicts instead for displacements

--- molssi/input_processor.py
+++ molssi/input_processor.py
@@ -5,6 +5,7 @@ A class for extracting information from the main input of the user
 from . import regex 
 import re
 from . import molecule
+import collections
 import ast
 
 class InputProcessor(object):
@@ -12,13 +13,15 @@ class InputProcessor(object):
     """
     def __init__(self, input_path):
         self.input_path = input_path
-        self.zmat_string = self.extract_zmat_string()
-        self.intcos_ranges = self.extract_intcos_ranges()
+        self.zmat_string = None 
+        self.intcos_ranges = None 
+        self.extract_zmat_string()
+        self.extract_intcos_ranges()
 
     def extract_zmat_string(self):
         with open(self.input_path, 'r') as f:
             self.full_string = f.read() 
-        return re.findall(regex.intcoords_regex, self.full_string)[0] 
+        self.zmat_string = re.findall(regex.intcoords_regex, self.full_string)[0] 
 
     def extract_intcos_ranges(self):
         """
@@ -27,11 +30,12 @@ class InputProcessor(object):
         # create molecule object to obtain coordinate labels
         mol = molecule.Molecule(self.zmat_string)
         geomlabels = mol.geom_parameters 
-        ranges = {}
+        ranges = collections.OrderedDict()
         # for every geometry label look for its range identifer, e.g. R1 = [0.5, 1.2]
         for label in geomlabels:
             match = re.search(label+"\s*=\s*(\[.+\])", self.full_string).group(1)
             ranges[label] = ast.literal_eval(match)
+        self.intcos_ranges = ranges
          
         
 
