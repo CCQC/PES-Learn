d1b0b9d56ddb0babe6652fc9cfbe556d9dfb9ab8
adabbott <adabbott@uga.edu>
2018-06-22 13:23:58 -0400

    switched to numpy based constants

--- MLChem/constants.py
+++ MLChem/constants.py
@@ -1,8 +1,8 @@
-import math
+import numpy as np
 
 # some constants used in the code
-rad2deg = 180.0 / math.pi
-deg2rad = math.pi / 180.0
+rad2deg = 180.0 / np.pi
+deg2rad = np.pi / 180.0
 
 bohr2angstroms = 0.52917720859
 hartree2ev =  27.21138505
