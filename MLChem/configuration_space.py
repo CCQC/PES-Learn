6b870eaa799da570fd891a3dbdeda12905b11ab1
adabbott <adabbott@uga.edu>
2018-06-26 13:14:37 -0400

    removed indexing from permutation applications, cleaned up code, added comments

--- MLChem/configuration_space.py
+++ MLChem/configuration_space.py
@@ -4,6 +4,10 @@ A class for building PES geometries
 
 from geometry_transform_helper import get_interatom_distances
 from permutation_helper import permute_bond_indices, induced_permutations
+import os
+import json
+import pandas as pd
+import numpy as np
 
 class ConfigurationSpace(object):
     """
@@ -19,21 +23,22 @@ class ConfigurationSpace(object):
         self.disps = self.input_obj.generate_displacements() 
         self.n_init_disps = len(self.disps)
 
-    def generate_geometry_dataframes(self):
-        print("Number of displacements without redundancy removal: {}".format(self.n_init_disps))
-        n_interatomics =  int(0.5 * (self.mol.n_atoms * self.mol.n_atoms - self.mol.n_atoms))
+    def generate_geometries(self):
+        print("Number of displacements: {}".format(self.n_init_disps))
+        n_atoms = self.mol.n_atoms - self.mol.n_dummy
+        n_interatomics =  int(0.5 * (n_atoms * n_atoms - n_atoms))
         print("Number of interatomic distances: {}".format(n_interatomics))
         self.bond_columns = []
         for i in range(n_interatomics):
             self.bond_columns.append("r%d" % (i))
         # preallocate df space, much faster
-        df = pd.DataFrame(index=np.arange(0, len(disps)), columns=self.bond_columns)
+        df = pd.DataFrame(index=np.arange(0, len(self.disps)), columns=self.bond_columns)
         # grab cartesians and internals 
         cartesians = []
         internals = []
-        for i, disp in enumerate(disps):
-            mol.update_intcoords(disp)
-            cart = mol.zmat2xyz()
+        for i, disp in enumerate(self.disps):
+            self.mol.update_intcoords(disp)
+            cart = self.mol.zmat2xyz()
             cartesians.append(cart)
             internals.append(disp)
             idm = get_interatom_distances(cart)
@@ -44,7 +49,7 @@ class ConfigurationSpace(object):
         df['internals'] = internals 
         self.all_geometries = df
 
-    def redundancy_removal(self):
+    def remove_redundancies(self):
         """
         Automated Redundant Geometry Removal
         Handles the removal of redundant geometries arising from 
@@ -55,22 +60,22 @@ class ConfigurationSpace(object):
         # (e.g., angular, dihedral equivalencies)
         self.unique_geometries = self.all_geometries.drop_duplicates(subset=self.bond_columns)
         # remove like-atom permutation duplicates
-        bond_indice_permutations = permute_bond_indices(mol.atom_count_vector)
-        bond_permutation_vectors = induced_permutations(mol.atom_count_vector, bond_indice_permutations) 
-        print("Interatomic distance equivalent permutations: ", bond_permutation_vectors)
+        bond_indice_permutations = permute_bond_indices(self.mol.atom_count_vector)
+        bond_permutation_vectors = induced_permutations(self.mol.atom_count_vector, bond_indice_permutations) 
+        print("Interatomic distances equivalent permutations: ", bond_permutation_vectors)
         for perm in bond_permutation_vectors:
             new_df = []
             permuted_rows = []
-            for row in self.unique_geometries.itertuples():
+            for row in self.unique_geometries.itertuples(index=False):
                 # apply induced bond permutation derived from like-atom permutations
-                # the first element of each row is the index, the last two are the cartesians and internals
-                new = [row[1:-2][i] for i in perm]  
+                # the first n rows are the interatomic distances which we want to permute, the last two rows are the cartesian and internal coordinates
+                new = [row[0:-2][i] for i in perm]  
                 permuted_rows.append(new)
                 # if its unaffected by the permutation, we want to keep one copy
-                if new == list(row[1:-2]):
+                if new == list(row[0:-2]):
                     new_df.append(row)
                 # uniqueness check
-                if list(row[1:-2]) not in permuted_rows:
+                if list(row[0:-2]) not in permuted_rows:
                     new_df.append(row)
             # update dataframe with removed rows for this particular permutation vector
             self.unique_geometries = pd.DataFrame(new_df)
@@ -79,11 +84,19 @@ class ConfigurationSpace(object):
 
 
     def generate_PES(self, template_obj):
+        # generate the full geometry set or the removed redundancy geometry set?
+        self.generate_geometries()
+        if self.input_obj.keywords['remove_redundancy'].lower() == 'true':
+            self.remove_redundancies()
+            df = self.unique_geometries 
+        elif self.input_obj.keywords['remove_redundancy'].lower() == 'false':
+            df = self.all_geometries
+          
         if not os.path.exists("./PES_data"):
             os.mkdir("./PES_data")
         os.chdir("./PES_data")
 
-        for i, cart_array in enumerate(self.unique_geometries['cartesians'], start=1):
+        for i, cart_array in enumerate(df['cartesians'], start=1):
             # build xyz input file
             xyz = ''
             xyz += template_obj.header_xyz()
@@ -94,11 +107,15 @@ class ConfigurationSpace(object):
             if not os.path.exists(str(i)):
                 os.mkdir(str(i))
             # grab original internal coordinates 
-            self.unique_geometries.iloc[i-1,-1].to_json("{}/internals".format(str(i)))
+            with open("{}/geom".format(str(i)), 'w') as f:
+                # preserve OrderedDict order by dumping as an iterable (list)
+                f.write(json.dumps([df.iloc[i-1,-1]])) 
             # grab interatomic distances 
-            self.unique_geometries.iloc[i-1,0:-2].to_json("{}/interatomics".format(str(i)))
+            #print(df.iloc[i-1,0:-2])
+            #print(df.iloc[i-1])
+            df.iloc[i-1,0:-2].to_json("{}/interatomics".format(str(i)))
             # write input file 
-            with open("{}/{}".format(str(i), input_obj.keywords['input_name']), 'w') as f:
+            with open("{}/{}".format(str(i), self.input_obj.keywords['input_name']), 'w') as f:
                 f.write(xyz)
         print("Your PES inputs are now generated. Run the jobs in the PES_data directory and then parse.")
         
