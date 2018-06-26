af7cc890eaa8b189b8a9dab8f18a136a9567a2d4
adabbott <adabbott@uga.edu>
2018-06-26 17:00:35 -0400

    added workaround for cclib energy inconsistencies

--- MLChem/outputfile.py
+++ MLChem/outputfile.py
@@ -72,21 +72,27 @@ class OutputFile(object):
         """
         cclib_outputobj = ccio.ccread(self.output_path) 
         e = None 
-        # for whatever reason, each energy is a single element list, and we only want the last one
+        # for whatever reason, sometimes each energy is a single element list, so we have to code around that 
         # also, cclib does not handle things well if something fails to parse, needtry/except pairs 
         if cclib_attribute == "scfenergies":
             try:
-                e = cclib_outputobj.scfenergies[-1][0] 
+                e = cclib_outputobj.scfenergies[-1] 
+                if isinstance(e, list):
+                    e = e[0]
             except:
                 e = None 
         if cclib_attribute == "mpenergies":
             try:
-                e = cclib_outputobj.mpenergies[-1][0] 
+                e = cclib_outputobj.mpenergies[-1] 
+                if isinstance(e, list):
+                    e = e[0]
             except:
                 e = None 
         if cclib_attribute == "ccenergies":
             try:
-                e = cclib_outputobj.ccenergies[-1][0] 
+                e = cclib_outputobj.ccenergies[-1] 
+                if isinstance(e, list):
+                    e = e[0]
             except:
                 e = None 
         # cclib puts energies into eV... ugh 
