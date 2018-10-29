88b2f72e4309dde9282fba09bb4520b16d689ff9
adabbott <adabbott@uga.edu>
2018-10-29 17:01:54 -0400

    separated data loading from init

--- MLChem/model.py
+++ MLChem/model.py
@@ -9,7 +9,7 @@ class Model(object):
 
     Parameters
     ----------
-    dataset : str 
+    dataset_path : str 
         A path to a potential energy surface file, which is readable by
         numpy.loadtxt() or pandas.read_csv()
 
@@ -20,18 +20,21 @@ class Model(object):
         InputProcessor object from MLChem. Used for keywords related to machine learning.
     """
     def __init__(self, dataset_path, ntrain, input_obj):
-        try:
-            data = np.loadtxt(dataset_path, delimiter=',', skiprows=1
-        except:   
-            raise Exception("Could not read dataset. Check to be sure the path is correct, 
-                                and it is a csv with the first line being column labels")
-        
-        self.dataset = data 
         self.ntrain = ntrain
         self.input_obj = input_obj
         self.hyperparameter_optimization = input_obj.keywords['hp_opt']
         self.hp_max_evals = input_obj.keywords['hp_max_evals']
+        # more keywords...
 
+    def load_dataset(self):
+        try:
+            #data = np.loadtxt(dataset_path, delimiter=',', skiprows=1
+            data = pd.read_csv(dataset_path)
+        except:   
+            raise Exception("Could not read dataset. Check to be sure the path is correct, 
+                                and it is a csv with the first line being column labels")
+        self.dataset = data 
+        return self.dataset
 
     def evaluate(self):
         pass
