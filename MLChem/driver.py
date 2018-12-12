d7d33cad9089a34dcf764e09a162a7c401a5f43d
adabbott <adabbott@uga.edu>
2018-12-12 12:00:55 -0500

    update driver, keywords

--- MLChem/driver.py
+++ MLChem/driver.py
@@ -19,13 +19,13 @@ with open('input.dat', 'r') as f:
     input_string = f.read()
 
 input_obj = MLChem.input_processor.InputProcessor(input_string)
-template_obj = MLChem.template_processor.TemplateProcessor("./template.dat")
 mol = MLChem.molecule.Molecule(input_obj.zmat_string)
 text = input("Do you want to 'generate' data, 'parse' data, or 'learn'?")
 start = timeit.default_timer()
 
 if text == 'generate':
     config = MLChem.configuration_space.ConfigurationSpace(mol, input_obj)
+    template_obj = MLChem.template_processor.TemplateProcessor("./template.dat")
     config.generate_PES(template_obj)
     print("Data generation finished in {} seconds".format(round((timeit.default_timer() - start),2)))
 
@@ -33,8 +33,9 @@ if text == 'parse':
     MLChem.parsing_helper.parse(input_obj, mol)
 
 if text == 'learn':
-    gp = MLChem.gaussian_process.GaussianProcess("PES.dat", input_obj, mol)
-    gp.optimize_model()
+    if input_obj.keywords["ml_model"] == 'gp':
+        gp = MLChem.gaussian_process.GaussianProcess(input_obj.keywords["pes_name"], input_obj, mol)
+        gp.optimize_model()
     
-stop = time.default_timer()
+stop = timeit.default_timer()
 print("Total run time: {} seconds".format(stop - start))
