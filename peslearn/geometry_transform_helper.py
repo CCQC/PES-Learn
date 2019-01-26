21e017dbc35a94a3e6e13f2bc8d5ccf623d066e3
adabbott <adabbott@uga.edu>
2019-01-26 18:05:09 -0500

    added support for reading in cartesian PES data

--- peslearn/geometry_transform_helper.py
+++ peslearn/geometry_transform_helper.py
@@ -3,7 +3,11 @@ Various functions for molecular geometry transformations
 """
 import math
 import numpy as np
+import pandas as pd
+import re
 from itertools import combinations
+from .regex import xyz_block_regex
+import collections
 
 def unit_vector(coords1, coords2):
     """
@@ -56,3 +60,78 @@ def get_interatom_distances(cart):
         #create lower triangle matrix
         matrix[j,i] = R
     return matrix
+
+def load_cartesian_dataset(xyz_path):
+    """
+    Loads a cartesian dataset with energies on their own line and with standard cartesian coordinates.
+    Reorganizes atoms into standard order (most common elements first, alphabetical tiebreaker)
+    """
+    xyz_re = xyz_block_regex
+
+    with open(xyz_path) as f:
+        data = f.read()
+    # extract energies
+    energies = re.findall("\n\s*(-?\d+\.\d+)\s*\n", data)
+    # extract geometries and clean up format
+    geoms = re.findall(xyz_re, data)
+    for i in range(len(geoms)):
+        geoms[i] = list(filter(None, geoms[i].split('\n')))
+    # find atom labels
+    sample = geoms[0]
+    atom_labels = [re.findall('\w+', s)[0] for s in sample]
+    natoms = len(atom_labels)
+    # convert atom labels to standard order (most common element first, alphabetical tiebreaker)
+    sorted_atom_counts = collections.Counter(atom_labels).most_common()
+    sorted_atom_counts = sorted(sorted_atom_counts, key = lambda x: (-x[1], x[0]))
+    sorted_atom_labels = []
+    for tup in sorted_atom_counts:
+        for i in range(tup[1]):
+            sorted_atom_labels.append(tup[0])
+    # find the permutation vector which maps unsorted atom labels to standard order atom labels
+    p = []
+    for i,j in enumerate(atom_labels):
+        for k,l in enumerate(sorted_atom_labels):
+            if j == l:
+                p.append(k)
+                sorted_atom_labels[k] = 'done'
+                continue
+    # permute all xyz geometries to standard order 
+    for g in range(len(geoms)):
+        geoms[g] = [geoms[g][i] for i in p]
+
+    # write new xyz file with standard order
+    with open('std_' + xyz_path, 'w+') as f:
+        for i in range(len(energies)):
+            f.write(energies[i] +'\n')
+            for j in range(natoms):
+                f.write(geoms[i][j] +'\n')
+
+    # remove everything from XYZs except floats and convert to numpy arrays
+    for i,geom in enumerate(geoms):
+        for j,string in enumerate(geom):
+            string = string.split()
+            del string[0] # remove atom label
+            geom[j] = np.asarray(string, dtype=np.float32)
+    
+    # convert to interatomic distances
+    final_geoms = []
+    for i in geoms:
+        idm = get_interatom_distances(i)
+        idm = idm[np.tril_indices(idm.shape[0],-1)]
+        final_geoms.append(idm)
+    
+    final_geoms = np.asarray(final_geoms)
+    energies = np.asarray(energies, dtype=np.float32)
+    
+    n_interatomics =  int(0.5 * (natoms * natoms - natoms))
+    bond_columns = []
+    for i in range(n_interatomics):
+        bond_columns.append("r%d" % (i))
+    DF = pd.DataFrame(data=final_geoms, columns=bond_columns)
+    DF['E'] = energies
+    DF.to_csv('interatomic_PES.dat',index=False, float_format='%12.10f')
+    return DF
+
+
+
+
--- peslearn/model.py
+++ peslearn/model.py
@@ -1,7 +1,13 @@
-from abc import ABC, abstractmethod
 from .data_sampler import DataSampler 
+from . import geometry_transform_helper as gth
+from .constants import hartree2cm, package_directory 
+from .regex import xyz_block_regex
+from abc import ABC, abstractmethod
+import re
 import pandas as pd
 import warnings
+import numpy as np
+import sklearn.metrics
 # GPy and sklearn output a bunch of annoying warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=RuntimeWarning)
@@ -20,18 +26,33 @@ class Model(ABC):
         A path to a potential energy surface file, which is readable as a
         pandas DataFrame by pandas.read_csv()
 
-    input_obj : MLChem object 
-        InputProcessor object from MLChem. Used for keywords related to machine learning.
+    input_obj : peslearn object 
+        InputProcessor object from peslearn. Used for keywords related to machine learning.
 
-    mol_obj : MLChem object 
-        Molecule object from MLChem. Used for molecule information if permutation-invariant geometry  representation is used.
+    mol_obj : peslearn object 
+        Molecule object from peslearn. Used for molecule information if permutation-invariant geometry representation is used.
     """
     def __init__(self, dataset_path, input_obj, mol_obj=None):
-        try:
-        # get PES data. #TODO relax formatting requirements, make more general
-            data = pd.read_csv(dataset_path)
-        except:   
-            raise Exception("Could not read dataset. Check to be sure the path is correct,and it is a csv with the first line being column labels.")
+        # load PES data, if interatomic distances CSV or standard cartesians
+        #TODO relax formatting requirements, make more general
+        # probe dataset to see if its cartesians or interatomics
+        with open(dataset_path) as f:
+            read = f.read()
+        if re.findall(xyz_block_regex, read):
+            data = gth.load_cartesian_dataset(dataset_path)
+        else:
+            try:
+                data = pd.read_csv(dataset_path)
+            except:   
+                raise Exception("Could not read dataset. Check to be sure the path is correct, and it is properly",
+                                "formatted. Can either be a csv-style file with the first line being a list of",
+                                "arbitrary geometry labels with last column labeled 'E', e.g.  r1,r2,r3,...,E")
+            #try:
+            #    data = gth.load_cartesian_dataset(dataset_path)
+            #except:   
+            #    raise Exception("Could not read dataset. Check to be sure the path is correct, and it is properly",
+            #                    "formatted. Can either be a csv-style file with the first line being a list of",
+            #                    "arbitrary geometry labels with last column labeled 'E', e.g.  r1,r2,r3,...,E")
 
         self.dataset = data.sort_values("E")
         self.n_datapoints = self.dataset.shape[0]
@@ -44,9 +65,14 @@ class Model(ABC):
             if self.mol:
                 self.pip = True
             else:
-                raise Exception("The use of permutation invariant polynomials ('use_pips' = true) requires Model objects are instantiated with a Molecule object: model = Model(dataset_path, input_obj, mol_obj)")
+                raise Exception(
+                "The use of permutation invariant polynomials ('use_pips' = true) requires Model objects are",
+                "instantiated with a Molecule object: model = Model(dataset_path, input_obj, mol_obj)")
         else:
-            print("Warning: Molecular geometry will not be transformed to permutation-invariant representation (either pes_format is not 'interatomics' or 'use_pips' = false). The model will therefore not generalize to symmetry-equivalent points. Ensure that the dataset is properly built to compensate for this.")
+            print(
+            "Warning: Molecular geometry will not be transformed to permutation-invariant representation", 
+            "(either pes_format is not 'interatomics' or 'use_pips' = false). The model will therefore not generalize", 
+            "to symmetry-equivalent points. Ensure that the dataset is properly built to compensate for this.")
 
         # keyword control
         self.ntrain = self.input_obj.keywords['training_points']
@@ -78,13 +104,51 @@ class Model(ABC):
         super().__init__()
 
     @abstractmethod
+    # all subclasses must have a build_model method.
     def build_model(self):
         pass
 
-    @abstractmethod
-    def predict(self):
-        pass
+    def predict(self, model, data_in):
+        prediction, v1 = model.predict(data_in, full_cov=False)
+        return prediction 
 
-    @abstractmethod
-    def compute_error(self):
-        pass
+    def compute_error(self, X, y, prediction, yscaler, max_errors=None):
+        """
+        Predict the root-mean-square error (in wavenumbers) of model given 
+        known X,y, a prediction, and a y scaling object, if it exists.
+        
+        Parameters
+        ----------
+        X : array
+            Array of model inputs (geometries)
+        y : array
+            Array of expected model outputs (energies)
+        prediction: array
+            Array of actual model outputs (energies)
+        yscaler: object
+            Sci-kit learn scaler object
+        max_errors: int
+            Returns largest (int) absolute maximum errors 
+
+        Returns
+        -------
+        error : float
+            Root mean square error in wavenumbers (cm-1)
+        """
+        if yscaler:
+            raw_y = yscaler.inverse_transform(y)
+            unscaled_prediction = yscaler.inverse_transform(prediction)
+            error = np.sqrt(sklearn.metrics.mean_squared_error(raw_y,  unscaled_prediction))
+            if max_errors:
+                e = np.abs(raw_y - unscaled_prediction) * hartree2cm
+                largest_errors = np.partition(e, -max_errors, axis=0)[-max_errors:]
+        else:
+            error = np.sqrt(sklearn.metrics.mean_squared_error(y, prediction))
+            if max_errors:
+                e = np.abs(y - prediction) * hartree2cm
+                largest_errors = np.partition(e, -max_errors, axis=0)[-max_errors:]
+        if max_errors:
+            return error, largest_errors
+        else:
+            return error
+
