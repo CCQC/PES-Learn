42b71dc33725e26b3b330c7dbce7262e296ba028
adabbott <adabbott@uga.edu>
2019-04-19 15:22:34 -0400

    added function to sort NAS layer trials by complexity

--- peslearn/ml/preprocessing_helper.py
+++ peslearn/ml/preprocessing_helper.py
@@ -81,4 +81,30 @@ def degree_reduce(raw_X, degrees):
         raw_X[:,i] = np.power(raw_X[:,i], 1/degree)
     return raw_X
 
+def sort_architectures(layers, inp_dim):
+    """
+    Takes a list of hidden layer tuples (n,n,n...) and input dimension size and
+    sorts it by the number of expected weights in the neural network
+    """
+    out_dim = 1
+    sizes = []
+    for struct in layers:
+        size = 0
+        idx = 0
+        size += inp_dim * struct[idx]
+        idx += 1
+        n = len(struct)
+        while idx < n:
+            size += struct[idx - 1] * struct[idx]
+            idx += 1
+        size += out_dim * struct[-1]
+        sizes.append(size)
+    sorted_indices = np.argsort(sizes).tolist()
+    layers = np.asarray(layers)
+    layers = layers[sorted_indices].tolist()
+    return layers
+
+
+
+
 
