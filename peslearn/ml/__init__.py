6b74c1d893ae58a60a5cebe310b7eda992533f2b
adabbott <adabbott@uga.edu>
2019-04-19 15:21:11 -0400

    added NeuralNetwork

--- peslearn/ml/__init__.py
+++ peslearn/ml/__init__.py
@@ -1,9 +1,11 @@
 from . import data_sampler
 from . import gaussian_process
+from . import neural_network 
 from . import preprocessing_helper 
 from . import model
 #from . import neural_network
 
 from .gaussian_process import GaussianProcess
 from .data_sampler import DataSampler
+from .neural_network import NeuralNetwork
 #from .neural_network import NeuralNetwork
