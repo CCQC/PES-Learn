8dac6d95afa4b9bb36ead98cf1eb243c5dddd119
adabbott <adabbott@uga.edu>
2019-05-14 15:36:31 -0400

    driver in 'learn' mode now no longer needs a molecule object (zmat string)

--- peslearn/driver.py
+++ peslearn/driver.py
@@ -15,7 +15,6 @@ with open('input.dat', 'r') as f:
     input_string = f.read()
 
 input_obj = peslearn.InputProcessor(input_string)
-mol = peslearn.datagen.Molecule(input_obj.zmat_string)
 
 if input_obj.keywords['mode'] == None:
     text = input("Do you want to 'generate' data, 'parse' data, or 'learn'?")
@@ -27,17 +26,24 @@ else:
 start = timeit.default_timer()
 
 if text == 'generate' or text == 'g':
+    mol = peslearn.datagen.Molecule(input_obj.zmat_string)
     config = peslearn.datagen.ConfigurationSpace(mol, input_obj)
     template_obj = peslearn.datagen.Template("./template.dat")
     config.generate_PES(template_obj)
     print("Data generation finished in {} seconds".format(round((timeit.default_timer() - start),2)))
 
 if text == 'parse' or text == 'p':
+    mol = peslearn.datagen.Molecule(input_obj.zmat_string)
     peslearn.utils.parsing_helper.parse(input_obj, mol)
 
 if text == 'learn' or text == 'l':
+    if input_obj.keywords['use_pips'] == 'true':
+        mol = peslearn.datagen.Molecule(input_obj.zmat_string)
     if input_obj.keywords["ml_model"] == 'gp':
-        gp = peslearn.ml.GaussianProcess(input_obj.keywords["pes_name"], input_obj, molecule_type=mol.molecule_type)
+        if input_obj.keywords['use_pips'] == 'true':
+            gp = peslearn.ml.GaussianProcess(input_obj.keywords["pes_name"], input_obj, molecule_type=mol.molecule_type)
+        else:
+            gp = peslearn.ml.GaussianProcess(input_obj.keywords["pes_name"], input_obj)
         gp.optimize_model()
     
 stop = timeit.default_timer()
