10e541e27255e36c9aea856cf155a47dd4d16892
adabbott <adabbott@uga.edu>
2019-07-10 14:42:40 -0400

    cleaned up ml files

--- peslearn/ml/__init__.py
+++ peslearn/ml/__init__.py
@@ -3,9 +3,7 @@ from . import gaussian_process
 from . import neural_network 
 from . import preprocessing_helper 
 from . import model
-#from . import neural_network
 
 from .gaussian_process import GaussianProcess
 from .data_sampler import DataSampler
 from .neural_network import NeuralNetwork
-#from .neural_network import NeuralNetwork
