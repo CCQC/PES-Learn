d7cbda726c7c64389c19ec623134025607932a5e
iantbeck <ian.beck888@gmail.com>
2024-03-04 13:44:49 -0500

    specified dtype for numpy asarray

--- peslearn/ml/preprocessing_helper.py
+++ peslearn/ml/preprocessing_helper.py
@@ -100,7 +100,7 @@ def sort_architectures(layers, inp_dim):
         size += out_dim * struct[-1]
         sizes.append(size)
     sorted_indices = np.argsort(sizes).tolist()
-    layers = np.asarray(layers)
+    layers = np.asarray(layers, dtype=object)
     layers = layers[sorted_indices].tolist()
     return layers
 
